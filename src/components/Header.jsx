4d25f67f93b93b987c6b1bda770a5c86682e743d
Manasa Vesala <vesalamanasa@gmail.com>
2019-02-15 10:53:34 -0800

    Working on home and Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 function Header(){
   var headerStyle={
-    backgroundColor:"aqua",
+    backgroundColor:"#f2656f",
     height:'50px',
     paddingTop:"25px",
     paddingLeft: "300px"
   }
   var astyle={
-    paddingLeft:"40px",
-    border:"2 black",
-    textDecoration:"none",
-    color:"black"
+    display: "inline",
+    paddingRight:30,
+    fontSize:30
   }
   return (
     <div style={headerStyle}>
-      <a style={astyle}><Link to="/">Home</Link></a>
-      <a style={astyle}><Link to="/about">About</Link></a>
-      <a style={astyle}><Link to="/projects">Projects</Link></a>
-      <a style={astyle}><Link to="/contact">Contact</Link></a>
+      <a style={astyle}><Link style={{textDecoration: 'none', color: 'white'}} to="/">Home</Link></a>
+      <a style={astyle}><Link style={{textDecoration: 'none', color: 'white'}} to="/about">About</Link></a>
+      <a style={astyle}><Link style={{textDecoration: 'none', color: 'white'}} to="/projects">Projects</Link></a>
+      <a style={astyle}><Link style={{textDecoration: 'none', color: 'white'}} to="/contact">Contact</Link></a>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import bg from '../assets/images/homebg2.jpg';
 
 function Home(){
+    var divStyle={
+        backgroundImage: "url("+bg+")",
+        height:800,
+        width:"100%",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+    }
+    var h1Style={
+        textAlign:"center",
+        fontSize:90,
+        paddingTop:100
+    }
+    var h3Style={
+        textAlign:"center",
+        fontSize: "30px",
+        paddingTop: 30,
+        margin:50
+    }
+    var h4Style={
+        textAlign:"center",
+        fontSize: "30px",
+        margin:50,
+        border: "1px solid darkorange",
+    }
   return (
-    <div>
-      <h1>Home</h1>
+    <div style={divStyle}>
+      <h1 style={h1Style}>Manasa Vesala</h1>
+      <h3 style={h3Style}>I'm a Seattle based web designer & front‑end developer focused on crafting clean  & user‑friendly experiences.</h3>
+      <h4 style={h4Style}>Contact Me</h4>
     </div>
   );
 }
